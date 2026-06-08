3a2feae104356c148caf10ee6c87e47c804c0fbf
Inge-Cesar <cesardev2913@gmail.com>
2026-06-08 13:01:02 -0400

    Fix indentation error in urls.py

--- core/urls.py
+++ core/urls.py
@@ -55,6 +55,7 @@ from django.conf import settings
 from django.urls import re_path
 from django.views.static import serve
 
+urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
